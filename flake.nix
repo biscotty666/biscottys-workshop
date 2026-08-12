1d33b39e95976cd70ab162385912977b14140129
biscotty666 <biscotty666@gmail.com>
2026-08-12 09:41:06 -0600

    Rewrote flake and added some packages;

--- flake.nix
+++ flake.nix
@@ -1,142 +1,166 @@
 {
+  description = "A basic flake with a shell";
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/25.11";
-
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/26.05";
+    systems.url = "github:nix-systems/default";
     flake-utils = {
       url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
     };
   };
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      #packageOverrides = pkgs.callPackage ./python-packages.nix {};
-      #python3 = pkgs.python3.override { inherit packageOverrides; };
-    in {
-      devShell = pkgs.mkShell {
-        name = "python-venv";
-        venvDir = "./.venv";
-        buildInputs = with pkgs; [
-            R
-            chromium
-            pandoc
-            texlive.combined.scheme-full
-            rstudio
-            quarto
-          (with rPackages; [
-            pagedown
-            tidyverse
-            sf
-            terra
-            leaflet
-            leaflet_extras
-            leaflet_extras2
-            maps
-            leafsync
-            elevatr
-            trackeR
-            zoo
-            patchwork
-            postcards
-            XML
-            xml2
-            gt 
-            gtExtras
-            janitor
-            nngeo
-            ggtext
-            zeallot
-            gpx
-            tidygeocoder
-            crsuggest
-            stplanr
-            rnaturalearth
-            rnaturalearthdata
-            quarto
-            osmdata
-            viridis
-            usethis
-            tmap
-            tidycensus
-            plotly
-            geodata
-            prettymapr
-            ggspatial
-            tidyterra
-            htmlwidgets
-            basemaps
-            trajr
-            webshot
-            reticulate
-          ])
-          (python3.withPackages(ps: with ps; [
-            ipython
-            pip
-            jupyter
-            widgetsnbextension
-            great-tables
-            googlemaps
-            ipympl
-            jupyter-nbextensions-configurator
-            jedi-language-server
-            osmnx
-            ipywidgets
-            libpysal
-            mypy
-            hvplot
-            pandas
-            us
-            seaborn
-            numpy
-            geopandas
-            geodatasets
-            pyogrio
-            geopy
-            matplotlib
-            pyproj
-            osmpythontools
-            folium
-            mapclassify
-            scipy
-            networkx
-            shapely
-            xarray
-#            rioxarray
-            dask
-            intake
-            intake-parquet
-            pooch
-            fiona
-            plotly
-            s3fs
-            rasterio
-          ]))
-        ];
-        postVenvCreation = ''
-          unset SOURCE_DATE_EPOCH
-          pip install -r requirements.txt
-        '';
 
-        shellHook = ''
-            export BROWSER=brave
-                # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
-    # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        myPackages = {
+          pythonEnv = pkgs.python3.withPackages (
+            ps: with ps; [
+              ipython
+              pip
+              jupyter
+              widgetsnbextension
+              great-tables
+              googlemaps
+              ipympl
+              jupyter-nbextensions-configurator
+              jedi-language-server
+              osmnx
+              ipywidgets
+              libpysal
+              mypy
+              hvplot
+              pandas
+              us
+              seaborn
+              numpy
+              geopandas
+              geodatasets
+              pyogrio
+              geopy
+              matplotlib
+              pyproj
+              osmpythontools
+              folium
+              mapclassify
+              scipy
+              networkx
+              shapely
+              xarray
+              rioxarray
+              dask
+              intake
+              intake-parquet
+              pooch
+              fiona
+              plotly
+              s3fs
+              rasterio
+              sympy
+              cement
+              # jpterm
+              # contextily
+              # movingpandas
+              # geoviews
+              # mapclassify
+              # stonesoup
+            ]
+          );
+          patchedQuarto = pkgs.quarto.overrideAttrs (oldAttrs: {
+            postPatch = (oldAttrs.postPatch or "") + ''
+              substituteInPlace bin/quarto.js \
+                --replace-fail "syntax-highlighting" "highlight-style"
+            '';
+          });
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          venvDir = "./.venv";
+          buildInputs = [
+            pkgs.fontconfig
+          ];
+          packages = builtins.attrValues {
+            inherit (myPackages)
+              patchedQuarto
+              pythonEnv
+              ;
+            inherit (pkgs)
+              R
+              # quarto
+              chromium
+              pandoc
+              rstudio
+              texliveMedium
+              ;
+            inherit (pkgs.rPackages)
+              pagedown
+              tidyverse
+              sf
+              terra
+              leaflet
+              # leaflet_extras
+              leaflet_extras2
+              maps
+              leafsync
+              elevatr
+              trackeR
+              zoo
+              patchwork
+              collapse
+              postcards
+              XML
+              xml2
+              gt
+              gtExtras
+              janitor
+              ggpubr
+              kit
+              microbenchmark
+              glue
+              rcompanion
+              ggridges
+              scales
+              classInt
+              spdep
+              nngeo
+              ggtext
+              zeallot
+              gpx
+              tidygeocoder
+              crsuggest
+              stplanr
+              rnaturalearth
+              rnaturalearthdata
+              quarto
+              osmdata
+              viridis
+              usethis
+              tmap
+              tidycensus
+              plotly
+              geodata
+              prettymapr
+              ggspatial
+              rstatix
+              tidyterra
+              htmlwidgets
+              basemaps
+              trajr
+              webshot
+              reticulate
+              ;
+          };
+          shellHook = ''
             export PIP_PREFIX=$(pwd)/venvDir
             export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
             export PATH="$PIP_PREFIX/bin:$PATH"
-            export QUARTO_PYTHON=/nix/store/2f2ymldccm86nqxlyhv76f5kjfy1h265-python3-3.12.9-env/bin/python
             unset SOURCE_DATE_EPOCH
-            #jupyter lab
-        '';
+          '';
 
-        postShellHook = ''
-          # allow pip to install wheels
-          unset SOURCE_DATE_EPOCH
-        '';
-      };
-    }
-  );
+        };
+      }
+    );
 }
